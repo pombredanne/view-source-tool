77929a7a0af3ae9a3da4000ded031614a85b2ec2
Zarino Zappia <mail@zarino.co.uk>
2013-01-20 21:56:22 +0000

    refactoring stuff out of the gigantic drillDown() function

--- js/view-source.js
+++ js/view-source.js
@@ -1,62 +1,72 @@
-function sift(object){
+Array.prototype.sift = function(){
   // filters non-truthy items out of an object
-  return $.grep(object, function(val, key){
+  return $.grep(this, function(val, key){
       return val
-  });
+  })
+}
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+}
+
+function generateFileList(path){
+  var ul = '<ul>'
+  if(path == '/'){
+    ul += '<li><a class="directory">bin</a></li>'
+    ul += '<li><a class="directory">dev</a></li>'
+    ul += '<li><a class="directory">etc</a></li>'
+    ul += '<li><a href="' + path + 'home/" class="directory">home</a></li>'
+    ul += '<li><a class="directory">lib</a></li>'
+    ul += '<li><a class="directory">root</a></li>'
+    ul += '<li><a class="directory">tmp</a></li>'
+    ul += '<li><a class="directory">usr</a></li>'
+    ul += '<li><a class="directory">var</a></li>'
+  } else if(path == '/home/'){
+    ul += '<li><a href="' + path + '.secret" class="file hidden">.secret</a></li>'
+    ul += '<li><a href="' + path + 'http/" class="directory">http</a></li>'
+    ul += '<li><a href="' + path + 'scraperwiki.json" class="file json">scraperwiki.json</a></li>'
+    ul += '<li><a href="' + path + 'scraperwiki.sqlite" class="file sqlite">scraperwiki.sqlite</a></li>'
+    ul += '<li><a href="' + path + 'scraper.py" class="file py">scraper.py</a></li>'
+    ul += '<li><a href="' + path + 'setup" class="file">setup</a></li>'
+  } else if(path == '/home/http/'){
+    ul += '<li><a href="' + path + 'css/" class="directory">css</a></li>'
+    ul += '<li><a href="' + path + 'img/" class="directory">img</a></li>'
+    ul += '<li><a href="' + path + 'index.html" class="file html">index.html</a></li>'
+    ul += '<li><a href="' + path + 'js/" class="directory">js</a></li>'
+  } else if(path == '/home/http/css/'){
+    ul += '<li><a href="' + path + 'screen.css" class="file css">screen.css</a></li>'
+  } else if(path == '/home/http/img/'){
+    ul += '<li><a href="' + path + 'loader.gif" class="file gif">loader.gif</a></li>'
+    ul += '<li><a href="' + path + 'bakground.png" class="file png">background.png</a></li>'
+  } else if(path == '/home/http/js/'){
+    ul += '<li><a href="' + path + 'script.js" class="file js">script.js</a></li>'
+  }
+  ul += '</ul>'
+  return ul
 }
 
 function drillDown(path, animate, callback){
   var duration = (animate == false) ? 0 : 300
-  var depth = sift(path.split('/')).length
+  var depth = path.split('/').sift().length
   var $s = $('<section>').css({
     left: $(window).width(),
     zIndex: depth,
     overflow: 'hidden'
-  })
-  $s.attr({
+  }).attr({
     'data-path': path,
     'data-depth': depth
   })
-  var $ul = $('<ul>')
-  if(path == '/'){
-    $ul.append('<li><a class="directory">bin</a></li>')
-    $ul.append('<li><a class="directory">dev</a></li>')
-    $ul.append('<li><a class="directory">etc</a></li>')
-    $ul.append('<li><a href="' + path + 'home/" class="directory">home</a></li>')
-    $ul.append('<li><a class="directory">lib</a></li>')
-    $ul.append('<li><a class="directory">root</a></li>')
-    $ul.append('<li><a class="directory">tmp</a></li>')
-    $ul.append('<li><a class="directory">usr</a></li>')
-    $ul.append('<li><a class="directory">var</a></li>')
-  } else if(path == '/home/'){
-    $ul.append('<li><a href="' + path + '.secret" class="file hidden">.secret</a></li>')
-    $ul.append('<li><a href="' + path + 'http/" class="directory">http</a></li>')
-    $ul.append('<li><a href="' + path + 'scraperwiki.json" class="file json">scraperwiki.json</a></li>')
-    $ul.append('<li><a href="' + path + 'scraperwiki.sqlite" class="file sqlite">scraperwiki.sqlite</a></li>')
-    $ul.append('<li><a href="' + path + 'scraper.py" class="file py">scraper.py</a></li>')
-    $ul.append('<li><a href="' + path + 'setup" class="file">setup</a></li>')
-  } else if(path == '/home/http/'){
-    $ul.append('<li><a href="' + path + 'css/" class="directory">css</a></li>')
-    $ul.append('<li><a href="' + path + 'img/" class="directory">img</a></li>')
-    $ul.append('<li><a href="' + path + 'index.html" class="file html">index.html</a></li>')
-    $ul.append('<li><a href="' + path + 'js/" class="directory">js</a></li>')
-  } else if(path == '/home/http/css/'){
-    $ul.append('<li><a href="' + path + 'screen.css" class="file css">screen.css</a></li>')
-  } else if(path == '/home/http/img/'){
-    $ul.append('<li><a href="' + path + 'loader.gif" class="file gif">loader.gif</a></li>')
-    $ul.append('<li><a href="' + path + 'bakground.png" class="file png">background.png</a></li>')
-  } else if(path == '/home/http/js'){
-    $ul.append('<li><a href="' + path + 'script.js" class="file js">script.js</a></li>')
+  if(path.endsWith('/')){
+    $s.html(generateFileList(path))
+  } else {
+    $s.html('loading file&hellip;')
   }
-  $ul.appendTo($s)
   $s.on('click', 'a', function(e){
     e.preventDefault()
     var sectionsAboveThisOne = $(this).parents('section').nextAll('section')
     if(sectionsAboveThisOne.length == 0){
       e.stopPropagation()
-      if($(this).hasClass('directory')){
-        drillDown($(this).attr('href'))
-      }
+      drillDown($(this).attr('href'))
     }
   })
   $s.on('click', function(){
